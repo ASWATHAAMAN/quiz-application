aafcc828dec45a787db76515aa6d49f5b3a5346b
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2024-01-08 18:45:40 +0530

    starts

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from "./Components";
+
+const App = () => {
+  return ( <Navbar/> );
+}
+ 
+export default App;
---
+++ src/Components/Navbar.components.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <main>
+      <div className="flex justify-between items-center mx-[2rem] my-[1rem]">
+        <h1 className="text-[26px] font-[700]">QUIZ APP</h1>
+        <span className="text-[20px] font-[500] cursor-pointer">Sign In</span>
+      </div>
+      <div className="flex justify-center items-center h-screen w-[100%]">
+        <button type="button" className="border-2 px-[1.5rem] py-[0.25rem] rounded-lg border-green-700 bg-green-700 text-white text-[18px] font-[700] font-serif hover:bg-green-500 hover:border-green-500">START</button>
+      </div>
+    </main>
+  );
+};
+
+export default Navbar;
