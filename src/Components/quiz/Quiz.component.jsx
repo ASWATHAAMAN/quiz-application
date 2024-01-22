a64cfc8401db5097f5dae07e59286706a3ea01c4
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2024-01-23 00:46:16 +0530

    api

--- src/Components/quiz/Quiz.component.jsx
+++ src/Components/quiz/Quiz.component.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+
 const Quiz = () => {
+  const [apiData, setApiData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const quizApi = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
+      try {
+        const response = await fetch(quizApi);
+        const data = await response.json();
+        setApiData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-    <h1>quiz</h1>
+      {apiData.results.map((result) => {
+        let options = result.incorrect_answers;
+        options.push(result.correct_answer)
+        const randomNumbers = Math.trunc(Math.random() * options.length)
+        options[randomNumbers]
+        return (
+          <div key={result.response_code}>
+            <p>{result.question}</p>
+            <input type="radio" id="optOne"/>
+<label htmlFor="optOne">{result.}</label>
+          </div>
+        );
+      })}
     </>
   );
-}
-  
+};
+
 export default Quiz;
