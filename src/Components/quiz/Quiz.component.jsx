963ee401e7d09052d5da73179bda2ed21413b994
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2024-01-23 23:55:18 +0530

    changes

--- src/Components/quiz/Quiz.component.jsx
+++ src/Components/quiz/Quiz.component.jsx
@@ -23,15 +23,32 @@ const Quiz = () => {
 
   return (
     <>
-      {apiData?.map((result,index) => {
+      {apiData?.map((result, index) => {
+        // let options = result.incorrect_answers;
+        // options.push(result.correct_answer);
+        // const randomNumbers = Math.trunc(Math.random() * options.length);
+        // options[randomNumbers];
+        const getRandom = (length) => {
+          console.log(length);
+          return Math.floor(Math.random() * Math.floor(length));
+        };
         let options = result.incorrect_answers;
-        options.push(result.correct_answer);
-        const randomNumbers = Math.trunc(Math.random() * options.length);
-        options[randomNumbers];
+        console.log(options);
+        options.splice(
+          Math.floor(getRandom(options.length), 0, result.correct_answer)
+        );
+
         return (
           <div key={index}>
             <p>{result.question}</p>
             <input type="radio" id="optOne" />
+            <label htmlFor="optOne">{options[0]}</label>
+            <input type="radio" id="optTwo" />
+            <label htmlFor="optTwo">{options[1]}</label>
+            <input type="radio" id="optThree" />
+            <label htmlFor="optThree">{options[2]}</label>
+            <input type="radio" id="optFour" />
+            <label htmlFor="optFour">{options[3]}</label>
           </div>
         );
       })}
